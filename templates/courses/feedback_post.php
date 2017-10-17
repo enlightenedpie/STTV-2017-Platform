bb56d1769f1c91df377d8df1460937cbee208258
David Paul <enlightenedpie@users.noreply.github.com>
2017-10-17 15:53:14 -0700

    Converted plain html to php

---
+++ templates/courses/feedback_post.php
@@ -0,0 +1,3 @@
+<h1><i class="material-icons">build</i>&nbsp;<i class="material-icons">send</i>&nbsp;<i class="material-icons">sentiment_very_dissatisfied</i></h1>
+<div class="col s12">Sorry, we're still working on this feature. Check back soon!</div>
+<?php echo STTV_VERSION; ?>
