dbfd506943bd45ce9a982ae2413fd28a1d30bf01
David Paul <enlightenedpie@users.noreply.github.com>
2017-10-17 15:55:16 -0700

    Initial commit / test Dates API

--- lib/account.php
+++ lib/account.php
@@ -1,119 +1,162 @@
 <?php
+if ( ! defined( 'ABSPATH' ) ) exit;
+/**
+ * SupertutorTV test dates API.
+ *
+ * REST endpoints for adding, displaying, and updating product reviews.
+ *
+ * @class 		STTV_Test_Dates
+ * @version		1.1.0
+ * @package		STTV
+ * @category	Class
+ * @author		Supertutor Media, inc.
+ */
 
-############################################
-##### ADD 'MY COURSES' TO ACCOUNT PAGE #####
-############################################
+class STTV_Test_Dates extends WP_REST_Controller {
+	
+	private $allowed_tests = array(
+		'act',
+		'sat',
+		'satsubject',
+		'satii',
+		'psat',
+		'gre',
+		'isee'
+	);
 
-class STTV_Add_Acct_Content {
-	/**
-	 * Custom endpoint name.
-	 *
-	 * @var string
-	 */
-	public static $endpoint = 'courses';
+	private $tests_table = STTV_PREFIX.'_tests_data';
 	
 	public function __construct() {
-		// Actions used to insert a new endpoint in the WordPress.
-		add_action( 'init', array( $this, 'add_endpoints' ) );
-		add_filter( 'query_vars', array( $this, 'add_query_vars' ), 0 );
-		
-		// Change the page title
-		add_filter( 'the_title', array( $this, 'sttv_endpoint_title' ) );
-		
-		// Insert the new tab/page into the My Account page.
-		add_filter( 'woocommerce_account_menu_items', array( $this, 'sttv_account_menu_items' ) );
-		add_action( 'woocommerce_account_' . self::$endpoint .  '_endpoint', array( $this, 'sttv_courses_endpoint_content' ) );
-	}
-	
-	
-	/**
-	 * Register new endpoint to use inside My Account page.
-	 *
-	 * @see https://developer.wordpress.org/reference/functions/add_rewrite_endpoint/
-	 */
-	public function add_endpoints() {
-		add_rewrite_endpoint( self::$endpoint, EP_ROOT | EP_PAGES );
-	}
-	
-	
-	/**
-	 * Add new query var.
-	 *
-	 * @param array $vars
-	 * @return array
-	 */
-	public function add_query_vars( $vars ) {
-		$vars[] = self::$endpoint;
-		return $vars;
-	}
-	
-	
-	/**
-	 * Set endpoint title.
-	 *
-	 * @param string $title
-	 * @return string
-	 */
-	public function sttv_endpoint_title($title) {
-		return wc_page_endpoint_title($title);
-	}
-	
-	public function sttv_account_menu_items( $items ) {
-		$items = array(
-		'dashboard'       => __( 'Dashboard', 'woocommerce' ),
-		self::$endpoint	  => __( 'My Courses', 'woocommerce' ),
-		'orders'          => __( 'Orders', 'woocommerce' ),
-		'downloads'       => __( 'Downloads', 'woocommerce' ),
-		'edit-address'    => __( 'Addresses', 'woocommerce' ),
-		'payment-methods' => __( 'Payment methods', 'woocommerce' ),
-		'edit-account'    => __( 'Account details', 'woocommerce' ),
-		'logout' 		  => __( 'Logout', 'woocommerce' ),
-	);
-		return $items;
-	}
-
-	
-	public function sttv_courses_endpoint_content() {
-		wc_get_template( 'myaccount/courses.php' );
-	}
-	
-}
-//new STTV_Add_Acct_Content;
-
-
-################################
-##### DISPLAY USER COURSES #####
-################################
-
-function sttv_get_user_courses() {
-	$status_query = array( 'user_id' => get_current_user_id(), 'type' => 'sensei_course_status' );
-	$user_courses_logs = Sensei_Utils::sensei_check_for_activity( $status_query , true );
-		if ( !is_array($user_courses_logs) ) {
-			 $user_courses_logs = array( $user_courses_logs );
+		global $wpdb;
+		if($wpdb->get_var("SHOW TABLES LIKE '$this->tests_table'") != $this->tests_table) {
+			$this->create_tests_table($wpdb);
 		}
-		$shmost = array();
-		foreach ($user_courses_logs as $course) {
-			$shmost[] = $course->comment_post_ID;
-		}
-		
-		$shmost = array_unique($shmost,SORT_REGULAR);
-		
-		$html = '<section class="my-account-courses"><h4>'.__('My Courses','woocommerce').'</h4>';
-		
-			foreach ($shmost as $post) {
-				$pp = get_post($post);
-				
-				$html .= '<article class="course-'.$pp->ID.' my-course">';
-					$html .= '<h5>'.$pp->post_title.'</h5>';
-				$html .= '</article>';
-			}
-	
-		$html .= '</section>';
-		print ($html);
-}
-add_action('woocommerce_account_dashboard','sttv_get_user_courses');
 
-function sttv_subs_in_dash() {
-	get_template_part('woocommerce/myaccount/my-subscriptions');
+		add_action( 'rest_api_init', array($this,'sttv_test_dates_api') );
+		
+		register_shutdown_function( array( $this, '__destruct' ) );
+	}
+	
+	public function __destruct() {
+        return true;
+    }
+	
+	public function sttv_test_dates_api() {
+		register_rest_route( STTV_REST_NAMESPACE, '/all_test_dates/',array(
+				array(
+					'methods' => 'POST',
+					'callback' => array($this,'update_test_dates')
+				)
+			)
+		);
+		register_rest_route( STTV_REST_NAMESPACE, '/test_dates/(?P<test>[a-zA-Z]+)',array(
+				array(
+					'methods' => 'GET',
+					'callback' => array($this,'get_test_dates'),
+					'args' => array(
+						'test' => array(
+							'validate_callback' => array($this,'is_allowed_test'),
+							'required' => true
+						)
+					)
+				),
+				array()
+			)
+	   );
+	}
+
+	public function is_allowed_test($val) {
+		return in_array($val, $this->allowed_tests);
+	}
+
+	public function get_test_dates($data) {
+		global $wpdb;
+		$test = esc_sql($data['test']);
+		$time = time();
+		return $wpdb->get_results( "SELECT * FROM $this->tests_table WHERE test = '$test' AND test_date > $time",OBJECT);
+	}
+
+	public function update_test_dates(WP_REST_Request $request) {
+		return $request->get_param('auth');
+	}
+
+	public function check_auth(WP_REST_Request $request, $action) {
+		return wp_verify_nonce($request->get_param('auth'),$action) && current_user_can('edit_test_dates_api');
+	}
+
+	private function create_tests_table($wpdb){
+		$charset_collate = $wpdb->get_charset_collate();
+		
+			$sql = "CREATE TABLE $this->tests_table (
+				 id int(10) NOT NULL AUTO_INCREMENT,
+				 test tinytext,
+				 test_date int(10) UNSIGNED,
+				 reg_date int(10) UNSIGNED,
+				 change_date int(10) UNSIGNED,
+				 late_reg text,
+				 reg_link varchar(255),
+				 description text,
+				 UNIQUE KEY id (id)
+			) $charset_collate;";
+			require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+			dbDelta( $sql );
+	}
 }
-add_action('woocommerce_account_dashboard','sttv_subs_in_dash');
+new STTV_Test_Dates;
+
+/**
+ * SupertutorTV test dates API admin area.
+ *
+ * Defines the Wordpress admin area and front-end editor for the STTV Test Dates API.
+ *
+ * @class 		STTV_Test_Dates_Admin
+ * @version		1.1.0
+ * @package		STTV
+ * @category	Class
+ * @author		Supertutor Media, inc.
+ */
+
+ class STTV_Test_Dates_Admin {
+
+	private $test_dates_cap = 'edit_test_dates_api';
+
+	public function __construct() {
+		add_action( 'admin_menu' , array($this,'test_dates_menu_page') );
+		add_action( 'admin_menu' , array($this,'define_test_dates_capabilities') );
+	}
+
+	public function define_test_dates_capabilities() {
+		// add new role specific to authorized API authors
+		add_role(
+			'test_dates_editor',
+			'Test Dates Editor',
+			[
+				'read'         => true,
+				$this->test_dates_cap   => true
+			]
+		);
+		
+		// add to admin role
+		$admin = get_role('administrator');
+		if (!in_array($this->test_dates_cap,$admin->capabilities)) {
+			$admin->add_cap($this->test_dates_cap, true);
+		}
+	}
+
+	public function test_dates_menu_page() {
+		add_submenu_page(
+			'edit.php?post_type=courses',
+			'Test Dates',
+			'Test Dates',
+			'manage_options',
+			STTV_PREFIX.'_test_dates', 
+			array($this, 'test_dates_admin')
+		);
+	}
+
+	public function test_dates_admin() {
+		print 'I did naht hit herrr, it\'s nahttrueit\'sbullshit I did nahhhhhhht. Oh hai Mark.';
+	}
+
+ }
+ new STTV_Test_Dates_Admin;
