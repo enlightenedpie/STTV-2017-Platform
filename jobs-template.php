bd3361019e2123534785dab2ec7313fbe6f5c443
David Paul <enlightenedpie@users.noreply.github.com>
2017-11-17 17:05:22 -0800

    Main Jobs page template

--- jobs-template.php
+++ jobs-template.php
@@ -2,9 +2,11 @@
 
 /* Template Name: Jobs Template */
 
-get_header(); 
+get_header();
+global $wp_query;
 ?>
 <?php get_template_part('templates/title'); ?>
 <section id="content-wrapper-full">
+<?php print_r($wp_query->query); print_r($stjob); ?>
 </section>
 <?php get_footer(); ?>
