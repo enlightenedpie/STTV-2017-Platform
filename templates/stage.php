4c3180058f2fc576382adde49ce7af673e3631be
David Paul <enlightenedpie@users.noreply.github.com>
2017-12-08 16:31:13 -0800

    Added stage action

--- templates/stage.php
+++ templates/stage.php
@@ -11,6 +11,7 @@ $ytlink = get_post_meta(get_the_ID(),'yt_link',true);
 				<iframe class="youtube-player" width="1920" height="1080" type="text/html" src="https://www.youtube.com/embed/<?php print $ytlink; ?>?version=3&rel=1&fs=1&autohide=2&showsearch=0&showinfo=1&iv_load_policy=1&wmode=transparent&playsinline=1" allowfullscreen="true" style="border:0;"></iframe>
 			</span><?php
 		endif;
+		do_action('sttv_stage_section');
 	elseif (is_404()) : ?>
 	<span class="stage-404"><?php
 		$img_id = get_theme_mod('404_stage_setting');
