646ad0587f5416248c10d6e880f317eccfcc3a7c
David Paul <enlightenedpie@users.noreply.github.com>
2017-12-18 15:56:34 -0800

    Reorganized REST classes

---
+++ lib/REST/init.php
@@ -0,0 +1,8 @@
+<?php 
+
+if ( ! defined( 'ABSPATH' ) ) {exit;}
+
+require('sttv_feedback.class.php');
+require('sttv_product_reviews.class.php');
+require('sttv_test_dates.class.php');
+require('sttv_jobs.class.php');
