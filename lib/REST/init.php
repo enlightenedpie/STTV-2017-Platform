1e54af227709694c2e77655ce6024d246e685c32
David Paul <enlightenedpie@users.noreply.github.com>
2017-12-22 17:10:40 -0800

    REST Rate limiter class and init

--- lib/REST/init.php
+++ lib/REST/init.php
@@ -2,6 +2,23 @@
 
 if ( ! defined( 'ABSPATH' ) ) {exit;}
 
+use STTV\REST\Limiter;
+
+require( __DIR__ . '/limiter/Limiter.php' );
+
+function sttvlimiter() {
+	static $instance;
+	if ( null === $instance ) {
+		$instance = new Limiter();
+	}
+	return $instance;
+}
+$sttvlimiter = sttvlimiter();
+
+add_action( 'rest_api_init', function() use ( $sttvlimiter ) {
+	$sttvlimiter->load();
+}, 5 );
+
 require('sttv_feedback.class.php');
 require('sttv_product_reviews.class.php');
 require('sttv_test_dates.class.php');
