e421f5d8a74d70bee6c2a5496dc80f0fc5835c8e
David Paul <enlightenedpie@users.noreply.github.com>
2018-01-03 15:34:23 -0800

    Updated methods and properties

--- lib/courses/sttv_course.class.php
+++ lib/courses/sttv_course.class.php
@@ -12,10 +12,110 @@
 class STTV_Course {
 	
 	public $id;
+
+	public $name;
+
+	public $slug;
+
+	public $link;
+
+	public $sales_page;
+
+	public $test;
+
+	public $intro;
+
+	public $description;
+
+	public $tl_content;
+
+	public $dashboard;
+
+	public $sections = [];
+
+	private $cap;
+
+	private $allcaps = [];
+
+	private $created;
+
+	private $modified;
 	
-	public function __construct($course = 0) {
+	private function __construct( $course ) {
 		$this->id = $course;
 		return $this;
 	}
+
+	private function set_name( $name ) {
+		$this->name = $name;
+		return $this;
+	}
+
+	private function set_slug( $slug ) {
+		$this->slug = $slug;
+		return $this;
+	}
+
+	private function set_link( $link ) {
+		$this->link = $link;
+		return $this;
+	}
 	
+	private function set_sales_page( $page ) {
+		$this->sales_page = $page;
+		return $this;
+	}
+
+	private function set_test( $test ) {
+		$this->test = $test;
+		return $this;
+	}
+
+	private function set_intro( $intro ) {
+		$this->intro = $intro;
+		return $this;
+	}
+
+	private function set_description( $desc ) {
+		$this->description = $desc;
+		return $this;
+	}
+
+	private function set_tl_content( $tlc ) {
+		$this->tl_content = $tlc;
+		return $this;
+	}
+
+	private function set_dashboard( $dash ) {
+		$this->dashboard = $dash;
+		return $this;
+	}
+
+	private function set_cap( $cap ) {
+		$this->cap = $cap;
+		return $this;
+	}
+
+	private function set_all_caps( $allcaps ) {
+		$this->allcaps = $allcaps;
+		return $this;
+	}
+
+	private function set_created( $cre ) {
+		$this->created = $cre;
+		return $this;
+	}
+
+	private function set_modified( $mod ) {
+		$this->modified = $mod;
+		return $this;
+	}
+} // end class.
+
+function sttvcourse( $id = 0 ) {
+	static $instance;
+	if ( null === $instance ) {
+		$instance = new STTV_Course( $id );
+	}
+	return $instance;
 }
