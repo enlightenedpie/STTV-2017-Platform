aad056fd1986ee0fa828b145bfff34b399c20ead
David Paul <enlightenedpie@users.noreply.github.com>
2018-01-24 15:55:23 -0800

    Webpack error fixed

--- s/ck.js
+++ s/ck.js
@@ -1,5 +1,11 @@
-module.exports = {
-    check : function(){
-        console.log('it works')
+//let setToken = require('setToken')
+
+var checkout = (function(){
+    return {
+        check : function(){
+            console.log('it works')
+        }
     }
-}
+})()
+
+module.exports = checkout
