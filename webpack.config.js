f531a9f08049a83546b7aad565449a379bb3b930
David Paul <enlightenedpie@users.noreply.github.com>
2018-01-24 15:01:58 -0800

    Webpack setup for checkout scripts

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+    entry: './s/checkout.min.js',
+    output: {
+        path: path.resolve(__dirname, ''),
+        filename: 'checkout.min.js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['es2015']
+                }
+            }
+        ]
+    },
+    stats: {
+        colors: true
+    },
+    devtool: 'source-map',
+    context: __dirname,
+    resolve: {
+        extensions: [ '.js', '.jsx', '.json' ],
+        modules: [ 'node_modules', path.resolve(__dirname, 's') ]
+    }
+};
