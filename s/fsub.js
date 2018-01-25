fcbf2e5db22325073bf5ee03e1685f7119810a05
JJ <jianan.beijing@gmail.com>
2018-01-25 11:11:16 -0800

    fix/require outcome and token into fsub

--- s/fsub.js
+++ s/fsub.js
@@ -1,4 +1,11 @@
-module.exports = {
+var setOutcome = require("./setOutcome");
+var setToken = require("./setToken");
+
+const fsub = {
   valid: false,
-  shipWasChecked: false
-};
+  shipWasChecked: false,
+  setOutcome: setOutcome,
+  setToken: setToken
+}
+
+module.exports = fsub;
