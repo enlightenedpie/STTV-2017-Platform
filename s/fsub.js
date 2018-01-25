e00ed890ff2faa98b6ddefea4c8da7b24e7688ef
JJ <jianan.beijing@gmail.com>
2018-01-25 10:40:01 -0800

    fix/fsub as object

--- s/fsub.js
+++ s/fsub.js
@@ -1,21 +1,23 @@
 var setOutcome = (function() {
   return {
-    var successElement = document.querySelector('.success');
-    var errorElement = document.querySelector('.error');
-    successElement.classList.remove('visible');
-    errorElement.classList.remove('visible');
+    setOutcome: function(){
+      var successElement = document.querySelector('.success');
+      var errorElement = document.querySelector('.error');
+      successElement.classList.remove('visible');
+      errorElement.classList.remove('visible');
 
-    fsub.valid = result.complete;
+      fsub.valid = result.complete;
 
-    var validSub = ($('#t_and_c').is(':checked') && result.complete);
-    $('.signup-submit').prop('disabled', !validSub);
+      var validSub = ($('#t_and_c').is(':checked') && result.complete);
+      $('.signup-submit').prop('disabled', !validSub);
 
-    if (typeof result.error !== 'undefined') {
-      $('.error').text(result.error.message);
-    } else {
-      $('.error').text('');
-    }
-  }
+      if (typeof result.error !== 'undefined') {
+        $('.error').text(result.error.message);
+      } else {
+        $('.error').text('');
+      };
+    }    
+  };
 })();
 
 module.exports = setOutcome;
