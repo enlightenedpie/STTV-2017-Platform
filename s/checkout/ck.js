40c2752ee6a14bda7584f4e68d564ce01e97fd30
David Paul <enlightenedpie@users.noreply.github.com>
2018-01-25 09:38:57 -0800

    Checkout redo beginnings

--- s/checkout/ck.js
+++ s/checkout/ck.js
@@ -2,8 +2,20 @@
 
 var checkout = (function(){
     return {
+        init: function(){
+            console.log( 'initialized' )
+        },
         check : function(){
             console.log('it works')
+        },
+        dataBind : function(t,cb){
+            try {
+                localStorage.setItem('checkout',$(t).attr('data-bind'))
+            } catch (e) {
+                document.cookie = 'checkout='+$(t).attr('data-bind')
+            } finally {
+                return typeof cb === 'function' && cb()
+            }
         }
     }
 })()
