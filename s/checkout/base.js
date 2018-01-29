2210219d385a2a5463edfee45c88e98ad4e3901e
David Paul <enlightenedpie@users.noreply.github.com>
2018-01-29 14:46:17 -0800

    Checkout object changed to variable

--- s/checkout/base.js
+++ s/checkout/base.js
@@ -1,9 +1,11 @@
-const checkout = require('ck');
+var checkout = require('ck');
 
 (function(c){
     if ( window.location.href.indexOf( 'checkout' ) !== -1 ){
         c.init()
     }
+    console.log(c);
+    // "Sign Up Now" button action
     $('.payment-launcher').on('click',function(e){
         e.preventDefault()
         var href = $(this).attr('href')
@@ -11,4 +13,10 @@ const checkout = require('ck');
             window.location = href
         })
     });
+
+    // test clicker
+    /* $(document).on('click',function(e){
+        e.preventDefault()
+        console.log(c)
+    }) */
 }(checkout));
