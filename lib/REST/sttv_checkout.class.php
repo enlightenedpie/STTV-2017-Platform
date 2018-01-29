06a14ee3125a66d7b2074d4a3dbe66510029e17f
David Paul <enlightenedpie@users.noreply.github.com>
2018-01-29 14:44:41 -0800

    Checkout REST class initila commit

---
+++ lib/REST/sttv_checkout.class.php
@@ -0,0 +1,17 @@
+<?php
+if ( ! defined( 'ABSPATH' ) ) exit;
+/**
+ * SupertutorTV checkout class.
+ *
+ * Properties, methods, and endpoints for the frontend checkout form to interact with.
+ *
+ * @class 		STTV_Checkout
+ * @version		1.4.0
+ * @package		STTV
+ * @category	Class
+ * @author		Supertutor Media, inc.
+ */
+
+ class STTV_Checkout extends WP_REST_Controller {
+
+ }
