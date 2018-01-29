2307222ef6ba717501c942b0c2319e7e7bde3df5
David Paul <enlightenedpie@users.noreply.github.com>
2018-01-29 14:46:31 -0800

    Added plan object

--- s/checkout/modules/subscriptions.js
+++ s/checkout/modules/subscriptions.js
@@ -2,8 +2,15 @@ var setOutcome = require("setOutcome");
 var setToken = require("setToken");
 
 module.exports = {
-  valid: true,
-  shipWasChecked: false,
+  plan : {
+    ID : 0,
+    name : '',
+    price : 0,
+    tax : 0,
+    shipping : 0,
+    disc : 0,
+    discp : 0
+  },
   setOutcome: setOutcome,
   setToken: setToken
 }
