a57c2437c3bd6891b74505c8db816f5e37295c35
David Paul <enlightenedpie@users.noreply.github.com>
2018-02-12 13:31:33 -0800

    Auth endpoints

--- lib/REST/sttv_auth.class.php
+++ lib/REST/sttv_auth.class.php
@@ -3,7 +3,7 @@ if ( ! defined( 'ABSPATH' ) ) exit;
 /**
  * SupertutorTV Authorization / Login / Logout class.
  *
- * Properties, methods, and endpoints for loggin in and out of the Supertutortv app.
+ * Properties, methods, and endpoints for logging in and out of the Supertutortv app.
  *
  * @class 		STTV_Auth
  * @version		1.4.0
@@ -13,8 +13,74 @@ if ( ! defined( 'ABSPATH' ) ) exit;
  */
 class STTV_Auth extends WP_REST_Controller {
 
-    public static function logout($cb){
+    public function __construct() {
+        add_action( 'rest_api_init', [ $this, 'register_auth_endpoints' ] );
+    }
+
+    public function register_auth_endpoints() {
+        register_rest_route( STTV_REST_NAMESPACE , '/auth', [
+            [
+                'methods' => 'POST',
+                'callback' => [ $this, 'sttv_auth_processor' ],
+                'args' => [
+                    'action' => [
+                        'required' => true,
+                        'type' => 'string',
+                        'description' => 'The type of auth action requested',
+                        'enum' => [
+                            'login',
+                            'logout'
+                        ]
+                    ]
+                ]
+            ]
+        ]);
+    }
+
+    public function sttv_auth_processor( WP_REST_Request $request ) {
+        $action = $request->get_param('action');
+        $auth = $request->get_header('X-STTV-Auth');
+
+        switch ($action) {
+            case 'login':
+                return $this->login($auth);
+                break;
+
+            case 'logout':
+                return $this->logout(home_url());
+                break;
+        }
+    }
+
+    private function login( $auth ) {
+        if ( null === $auth || empty($auth) ) {
+            $data = [
+				'code'    => 'auth_header',
+				'message' => 'You must set the authentication header X-STTV-Auth',
+				'data'    => [ 
+					'status' => 401
+				]
+			];
+			return new WP_REST_Response( $data, 401 );
+        }
+
+        $auth = explode( ':', base64_decode($auth) );
+
+        $user = $auth[0];
+        unset($auth[0]);
+        $pw = implode( ':', $auth);
+
+        return wp_signon([
+            'user_login'=>$user,
+            'user_password'=>$pw,
+            'remember'=>true
+        ],
+        true);
+    }
+
+    private function logout( $redirect = '' ){
         wp_logout();
-        if (is_callable($cb)){$cb();}
+        return $redirect;
     }
 }
+new STTV_Auth;
