28f16bd7bc5372e6a7f5bae7bc1fdeabb141c153
David Paul <enlightenedpie@users.noreply.github.com>
2018-02-15 10:48:43 -0800

    Login form / scripts update

--- sttv-js.js
+++ sttv-js.js
@@ -5,8 +5,31 @@ var closerFunc = function(callback) {
 	if (typeof callback == 'undefined') {callback = function(){return true;}}
 	callback();
 };
+
 ( function ( $ ) { //begin wrapper
 	"use strict";
+
+var _st = {
+	request : function(obj) {
+		var ajaxp = {
+			url: obj.route || '',
+			method: obj.method || 'GET',
+			headers: obj.headers || {},
+			processData : false,
+			dataType : obj.dataType || 'json',
+			success: function(data){
+				typeof obj.success !== 'undefined' && obj.success(data);
+			},
+			error: function(x,s,r){
+				typeof obj.error !== 'undefined' && obj.error([x,s,r]);
+			}
+		}
+		if (ajaxp.method !== 'GET') {
+			ajaxp['data'] = JSON.stringify(obj.cdata || {})
+		}
+		$.ajax(ajaxp)
+	}
+}
 
 // Opener functions
 
@@ -48,33 +71,51 @@ $('li.menu-item-has-children>a').click(function(e) {
 
 $('form#sttv_login_form').on('submit',function(e) {
 	e.preventDefault();
-	var loader = '<img src="'+sttvAjax.contentURL+'/i/sttv-spinner.gif" alt="Loading..." />';
-	var ld = $('#loading_overlay');
-		ld.html(loader).promise().done(function(){
-			$(this).fadeIn(250);
-		});
-		
-		
-	var formData = {
-		action: 'sttvlogin',
-		nonce:  this.sttv_nonce_ver.value,
-		sttv_login: this.sttv_user.value,
-		sttv_pass: this.sttv_pass.value,
-		g_recaptcha_response: grecaptcha.getResponse()
-	};
-	
-	$.post(sttvAjax.ajaxURL, formData, function(data){
-			console.log(data);
-			if(data.success === false){
-				ld.fadeOut(250);
-				$('p.error').html(data.data.message).css('display','block');
-			} else {
-				ld.empty().html('<p class="sblock"><strong><i class="material-icons">done</i></strong></p>').fadeIn(250);
-				$('.sblock').hide().fadeIn(250);
-					setTimeout(function(){window.location.replace('/my-account');},3000);
+
+	var loader = '<img src="'+stajax.contentURL+'/i/sttv-spinner.gif" alt="Loading..." />',
+		ld = $('.loading_overlay');
+	ld.append(loader).promise().done(function(){
+		$(this).fadeIn(250);
+	});
+
+	_st.request({
+		route : stajax.rest.url+'/auth?action=login',
+		method : 'POST',
+		headers : {
+			'X-WP-Nonce' : stajax.rest.nonce,
+			'X-STTV-Auth' : btoa(this.sttv_user.value+':'+this.sttv_pass.value)
+		},
+		success : function(data) {
+			if ( data.code == 'login_success' ) {
+				ld.empty().html('<p class="sblock"><strong><i class="material-icons">done</i></strong></p>').fadeIn(250)
+				$('.sblock').hide().fadeIn(250)
+				setTimeout(function(){
+					window.location.href = data.data.redirect
+				},250);
 			}
+		},
+		error : function(x) {
+			var data = x[0].responseJSON,
+				msg = ( typeof data.data.errors !== 'undefined') ? data.data.errors.too_many_retries[0] : data.message;
+
+			$('.message').html(msg)
+			ld.fadeOut(250)
+			console.log(data)
+		}
+	})
+	
+	/*$.post(sttvAjax.ajaxURL, formData, function(data){
+		console.log(data);
+		if(data.success === false){
 			
-		});
+			$('p.error').html(data.data.message).css('display','block');
+		} else {
+			
+			
+				
+		}
+		
+	}); */
 });
 
 } ( jQuery ) ); //end wrapper
