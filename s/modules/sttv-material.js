79ff6676143fe5c8b0b8d963e85b7bcd4b4def39
David Paul <enlightenedpie@users.noreply.github.com>
2018-02-26 09:37:56 -0800

    Removed logout script from header

--- s/modules/sttv-material.js
+++ s/modules/sttv-material.js
@@ -1,16 +1,8 @@
 ( function ( $ ) { //begin wrapper
 	"use strict";
 	
-	
 	$(document).ready(function() {
     	$('select').material_select();
 	  });
-	  
-	$('#logout-btn a').click(function(e){
-		e.preventDefault()
-		$.post(stajax.rest.url+'/auth?action=logout',function(d){
-			window.location.href = d
-		});
-	})
   
   } ( jQuery ) ); //end wrapper
