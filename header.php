48a8a37eb637282fd2736b5f04adc5f48e8eae8d
David Paul <enlightenedpie@users.noreply.github.com>
2018-04-06 14:53:31 -0700

    Modal reworkings for account/login

--- header.php
+++ header.php
@@ -4,9 +4,8 @@
 
 <?php 
 	global $current_user;
-	$firstname = get_user_meta($current_user->ID,'first_name',true);
-	$text = is_user_logged_in() ? $firstname : 'Login';
-	//print_r($text);
+    $firstname = get_user_meta($current_user->ID,'first_name',true);
+    $loggedin = is_user_logged_in();
 ?>
     <header class="z-depth-1" id="siteheader">
         <a href="#" id="main-menu" class="slide-bar">
@@ -16,18 +15,17 @@
             </div>
         </a>
         <div id="logo-box">
-        	<a href="<?php echo esc_url( home_url( '/' ) ); ?>">
+        	<a href="<?php echo site_url(); ?>">
             	<img src="<?php header_image(); ?>" alt="<?php echo bloginfo('name');?>" />
             </a>
         </div>
-        <a href="<?php echo site_url(); ?>/my-account" id="login">
+        <a href="/" id="login" class="modal-toggle" <?php print ($loggedin) ? 'data-action="account"' : 'data-action="login"'; ?>>
             <div class="icon-block">
                 <span class="dashicons dashicons-admin-users"></span>
-                <span class="link-text"><?php print $text; ?></span>
+                <span class="link-text"><?php print ($loggedin) ? $firstname : 'Login'; ?></span>
             </div>
         </a>
     </header>
-<?php get_template_part('templates/account'); ?>
     <div id="not-header-wrapper" class="row">
     <?php if (is_singular('courses')) : ?>
     	<div id="courses-left-wrapper" class="col m12 l9">
