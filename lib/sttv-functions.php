24e2835ca9b85a638da8c75ea08feb710aeb71b4
David Paul <enlightenedpie@users.noreply.github.com>
2018-04-06 14:52:40 -0700

    Reorganized code instantiation

--- lib/sttv-functions.php
+++ lib/sttv-functions.php
@@ -33,31 +33,6 @@ function is_child($page = '') {
 	endif;
 }
 
-function sttv_login_form() { ?>
-	<div id="form-wrapper">
-	<div class="loading_overlay"></div>
-	<div id="form-identity">
-		<img src="<?php print get_header_image(); ?>" alt="Login form header" />
-	</div>
-	<form id="sttv_login_form" action="/" method="post">
-		<p class="message"></p>
-		<div class="row">
-			<div class="input-field s12">
-				<input type="text" name="sttv_user" id="sttv_user" minlength="4" />
-				<label for="sttv_user" data-error="Must be at least 4 characters" data-success="Good job!">Username</label>
-			</div>
-			<div class="input-field s12">
-				<input type="password" name="sttv_pass" id="sttv_pass" />
-				<label for="sttv_pass">Password</label>
-			</div>
-		</div>
-		<button type="submit" class="z-depth-1" id="login-btn">Login</button>
-		<input type="hidden" name="whichform" value="login" />
-	</form>
-	<div id="forgot-block"><a class="lostpwd" href="<?php print wp_lostpassword_url(); ?>">Forgot your password?</a></div>
-	</div><?php 
-}
-
 function sttv_name_generate() {
 	//delete_transient('_courses_dynamic_js');
 	$transient = get_transient('_courses_dynamic_js');
@@ -88,12 +63,12 @@ function sttv_name_generate() {
 	wp_enqueue_script( 'sttv-course-object', get_stylesheet_directory_uri().'/s/courses/'.$filename, 'jquery', null, false);
 }
 
-function sttv_get_template($temp,$dir='',$object=null) {
+function sttv_get_template($temp,$dir='',$sgt=null) {
 	$dir = (!empty($dir))?"{$dir}/":"";
-	$path = get_template_directory().'/templates/'.$dir.$temp;
+	$path = STTV_TEMPLATE_DIR.$dir.$temp;
 	$extension = file_exists($path.'.php') ? '.php': '.html';
 	
-	return (include $path.$extension);
+	require $path.$extension;
 }
 
 function sttv_array_map_recursive($callback, $array) {
@@ -112,7 +87,10 @@ function sttv_uid ( $prefix = '', $random = '', $entropy = false, $length = 0 ){
 	return substr( $string, 0, ($length ?: strlen($string)) );
 }
 
-add_action('stripepress_events_invalid','sttv_404_redirect');
+function sttv_verify_rest_nonce( WP_REST_Request $request ) {
+	return wp_verify_nonce( $request->get_header('X-WP-Nonce'), STTV_REST_AUTH );
+}
+
 function sttv_404_redirect() {
 	global $wp_query;
 	$wp_query->set_404();
