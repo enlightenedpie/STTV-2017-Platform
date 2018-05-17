419d2be1b3587b4c1a251ec946d8607350e07ee6
David Paul <enlightenedpie@users.noreply.github.com>
2018-05-17 16:48:52 -0700

    More redesign stylings

--- templates/stage.php
+++ templates/stage.php
@@ -15,16 +15,15 @@ $ytlink = get_post_meta(get_the_ID(),'yt_link',true);
 	
 		return false;
 	
-	elseif ( is_singular() ) : ?>
-	<?php
+	elseif ( is_singular() ) :
 		
 		if (!empty($ytlink)) : ?>
-			<span class="sttv-embed-video" style="text-align:center; display: block;">
+			<div class="sttv-embed-video col s12 xl6 push-xl6">
 				<iframe class="youtube-player" width="1920" height="1080" type="text/html" src="https://www.youtube.com/embed/<?php print $ytlink; ?>?version=3&rel=1&fs=1&autohide=2&showsearch=0&showinfo=1&iv_load_policy=1&wmode=transparent&playsinline=1" allowfullscreen="true" style="border:0;"></iframe>
-			</span><?php
+			</div><?php
 
 		elseif (has_post_thumbnail()) : ?>
-			<div id="cover-img" class="col l12 xl6 push-xl6" style="background-image: url(<?php print get_the_post_thumbnail_url( get_the_ID(), 'full' ); ?>); background-position: 49% 29% !important"></div>
+			<div id="cover-img" class="col s12 xl6 push-xl6" style="background-image: url(<?php print get_the_post_thumbnail_url( get_the_ID(), 'full' ); ?>); background-position: 49% 29% !important"></div>
 		<?php
 
 		endif; ?>
