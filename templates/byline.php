144688a434c44b4b4dfec9c805aa14f56badb2b7
David Paul <enlightenedpie@users.noreply.github.com>
2018-05-26 07:44:10 -0700

    Added byline filter

--- templates/byline.php
+++ templates/byline.php
@@ -1,10 +1,5 @@
 <?php
 
-printf( 
-    '<span itemprop="author" itemscope="" itemtype="http://schema.org/Person" class="author byline">%1$s <a itemprop="name" href="%2$s" title="%3$s" rel="author">%4$s</a> | %5$s</span>', 
-    esc_html__( 'By', 'wordpress-seo' ),
-    esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ),
-    esc_attr( sprintf( __( 'More about %s', 'wordpress-seo' ), get_the_author() ) ),
-    get_the_author(),
-    get_the_date()
-);
+$byline = '<span itemprop="author" itemscope="" itemtype="http://schema.org/Person" class="author byline">By <a itemprop="name" href="'.esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ).'" title="More about '.get_the_author().'" rel="author">'.get_the_author().'</a> | '.get_the_date().'</span>';
+
+print apply_filters( 'sttv_byline', $byline );
