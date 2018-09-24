4f0e30db9dcce848a5a99c0d173ca2836fa71dd0
David Paul <enlightenedpie@users.noreply.github.com>
2018-09-24 12:20:42 -0700

    whatever

--- scripts/modules/signup/cardSetup.js
+++ scripts/modules/signup/cardSetup.js
@@ -1,9 +1,7 @@
 export default function cardSetup() {
     this.state.stripe = Stripe(_st.stripe.publicKey)
     this.state.elements = this.state.stripe.elements()
-    this.state.card.obj = this.state.elements.create('card',{
-        hidePostalCode: true
-    })
+    this.state.card.obj = this.state.elements.create('card')
     this.state.card.obj.mount('#stSignupCardElement')
     this.state.card.obj.on( 'change', ( event ) => {
         this.setOutcome( event )
