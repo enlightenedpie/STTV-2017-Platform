f351a22f372498880ce11d2b2ab43ef79c0a674b
David Paul <enlightenedpie@users.noreply.github.com>
2018-11-25 09:55:48 -0800

    Updated login template with alert

--- templates/auth/_authform.php
+++ templates/auth/_authform.php
@@ -5,6 +5,9 @@
 	</div>
 	<form id="sttv_login_form" action="/" method="post">
 		<p class="message"></p>
+		<div class="row" style="color:white;background-color:red;padding:1em">
+			<span>ATTENTION: If you purchased one of our courses after November 19th, 2018, DO NOT sign in below. Click <a href="https://courses.supertutortv.com/login">here</a> for the proper signin page.</span>
+		</div>
 		<div class="row">
 			<div class="input-field s12">
 				<input type="text" name="sttv_user" id="sttv_user" minlength="4" />
