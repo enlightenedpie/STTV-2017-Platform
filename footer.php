2eebccc5ff384b04bf592923c221e3382e3ab339
David Paul <enlightenedpie@users.noreply.github.com>
2018-11-25 12:55:56 -0800

    style update

--- footer.php
+++ footer.php
@@ -5,6 +5,15 @@
    <div id="courses-right-sidebar" class="col m12 l3"></div>
     <?php endif; ?>
 </div><?php //end #not-header-wrapper ?>
+<style type="text/css">
+footer a {
+    text-decoration: underline
+}
+
+footer a:hover {
+    color: #109fda
+}
+</style>
 <div id="pre-footer-cta"><?php do_action('sttv_pre_footer'); ?></div>
     <footer class="z-depth-3">
         <small>©<?php echo date('Y'); ?> Supertutor Media, Inc. All Rights Reserved.</small><br/>
