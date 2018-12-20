5684db72cabc5f3c6cd28cc95c78c5e8c05fba30
David Paul <enlightenedpie@users.noreply.github.com>
2018-12-20 15:28:26 -0800

    gjgdgdngdn

--- migrateusers.php
+++ migrateusers.php
@@ -11,15 +11,7 @@ $users = get_users([
     'role__in' => [ 'the_best_act_prep_course_ever' ]
 ]);
 
-$data = serialize($users[0]);
-
-/* $response = wp_remote_post('https://dev.api.supertutortv.com/v2/migrate/users',[
-    'headers' => [
-        'Content-Type' => 'text/plain',
-        'STTVWHSEC' => hash_hmac( 'sha256', $data, STTV_WHSEC )
-    ],
-    'body' => $data
-]); */
+$data = serialize($users);
 
 $curl = curl_init();
 
@@ -40,5 +32,5 @@ $resp = curl_exec($curl);
 curl_close($curl);
 
 echo '<pre>';
-print_r( json_decode($resp) );
+print_r( $resp );
 echo '</pre>';
