c304827d38996349a4ebe29cf886b5d8fc8a0f09
David Paul <enlightenedpie@users.noreply.github.com>
2018-12-20 15:02:29 -0800

    curl updates to migration script

--- migrateusers.php
+++ migrateusers.php
@@ -13,14 +13,32 @@ $users = get_users([
 
 $data = serialize($users[0]);
 
-$response = wp_remote_post('https://dev.api.supertutortv.com/v2/migrate/users',[
+/* $response = wp_remote_post('https://dev.api.supertutortv.com/v2/migrate/users',[
     'headers' => [
         'Content-Type' => 'text/plain',
         'STTVWHSEC' => hash_hmac( 'sha256', $data, STTV_WHSEC )
     ],
     'body' => $data
-]);
+]); */
+
+$curl = curl_init();
+
+curl_setopt_array($curl, array(
+    CURLOPT_RETURNTRANSFER => 1,
+    CURLOPT_URL => 'https://dev.api.supertutortv.com/v2/migrate/users',
+    CURLOPT_USERAGENT => 'STTV Main Site Migration',
+    CURLOPT_HTTPHEADER => [
+        'Content-Type' => 'text/plain',
+        'STTVWHSEC' => hash_hmac( 'sha256', $data, STTV_WHSEC )
+    ],
+    CURLOPT_POST => 1,
+    CURLOPT_POSTFIELDS => $data
+));
+
+$resp = curl_exec($curl);
+
+curl_close($curl);
 
 echo '<pre>';
-print_r( $response );
+print_r( $resp );
 echo '</pre>';
