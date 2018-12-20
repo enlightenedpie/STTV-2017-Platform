3d8197ad58dd1103efe13fdcd6b8103de9f6f93f
David Paul <enlightenedpie@users.noreply.github.com>
2018-12-20 14:26:33 -0800

    updating migration script

--- migrateusers.php
+++ migrateusers.php
@@ -9,4 +9,16 @@ current_user_can('manage_options') || exit;
 
 $users = get_users([ 'role__in' => [ 'the_best_act_prep_course_ever' ] ]);
 
-print serialize($users[0]);
+$data = serialize($users[0]);
+
+$response = wp_remote_post('https://dev.api.supertutortv.com/v2/migrate/users',[
+    'headers' => [
+        'Content-Type' => 'text/plain',
+        'STTVWHSEC' => hash_hmac( 'sha256', $data, STTV_WHSEC )
+    ],
+    'body' => $data
+]);
+
+echo '<pre>';
+print_r( $response );
+echo '</pre>';
